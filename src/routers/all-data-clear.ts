5bd310d6ef8d3674f54a1939884b86e0d8353389
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2023-10-10 21:27:08 +0300

    3 push

--- src/routers/all-data-clear.ts
+++ src/routers/all-data-clear.ts
@@ -2,13 +2,13 @@ import {Request, Response, Router} from "express";
 import {app, HTTP_STATUS} from "../index";
 import {dbVideos} from "../db-items/db-videos";
 import {dbBlogsPosts} from "../db-items/db-blogs-posts";
+import {dataBlog, dataPost} from "../DB/data-base";
 
 
 export const AllDataClear = Router();
 
 AllDataClear.delete('/', (req:Request, res: Response) => {
-    dbVideos.videos = []
-    dbBlogsPosts.blogs = []
-    dbBlogsPosts.posts = []
+    dataPost.deleteMany({})
+    dataBlog.deleteMany({})
     res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
 })
