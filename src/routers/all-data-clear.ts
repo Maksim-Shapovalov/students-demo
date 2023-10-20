f450bae698686a610b89f242c62fa69a6999b30b
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2023-10-20 17:20:51 +0300

    hw5-commit -13

--- src/routers/all-data-clear.ts
+++ src/routers/all-data-clear.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router} from "express";
 import {app, HTTP_STATUS} from "../index";
 import {dbVideos} from "../db-items/db-videos";
 import {dbBlogsPosts} from "../db-items/db-blogs-posts";
-import {dataBlog, dataPost} from "../DB/data-base";
+import {dataBlog, dataPost, dataUser} from "../DB/data-base";
 
 
 export const AllDataClear = Router();
@@ -10,5 +10,6 @@ export const AllDataClear = Router();
 AllDataClear.delete('/', (req:Request, res: Response) => {
     dataPost.deleteMany({})
     dataBlog.deleteMany({})
+    dataUser.deleteMany({})
     res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
 })
